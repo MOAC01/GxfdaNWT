8aa537df0e2266bc8bbb14d4611e109436056baf
moac <uysctx86asm@foxmail.com>
2020-03-11 08:47:07 +0800

    增加广播接收器，检测WiFi状态

--- app/src/main/java/com/moac/MainActivity.java
+++ app/src/main/java/com/moac/MainActivity.java
@@ -3,136 +3,134 @@ package com.moac;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TelnetUtil telnetUtil;
     private EditText editText;
     private Button search;
-    private ProgressBar progressBar;
-    private Map<String,String> ipaddress=new HashMap<>();
+    private TextView wlan_state;
+    private WlanReceiver wlanReceiver;
+    boolean isRegReceiver=false;
 
     private Handler uiHandler=new Handler(){
         @Override
         public void handleMessage(@NonNull Message msg) {
             switch (msg.what){
+                case 0:
+                    wlan_state.setText("WiFi已关闭");
+                    search.setEnabled(false);
+                    break;
                 case 1:
-                    messageBox("连接失败，请检查网络","好的",true);
                     break;
-                case 2:
-                    alert("断开连接失败，可能已经断开或未连接",1);
-                    break;
-                case 3:
-                    messageBox("读取过程中错误","好的",false);
-                    break;
-                case 4:
-                    messageBox("操作超时","好的",false);
-                    break;
-                case 5:
-                    progressBar.setVisibility(View.INVISIBLE);
-                    editText.setEnabled(true);
-                    search.setEnabled(true);
-                    alert("查找完成",0);
+                case -1:
+                    wlan_state.setText("WiFi已断开");
                     break;
 
+                case 2:
+                    wlan_state.setText("WiFi已连接");
+                    search.setEnabled(true);
+                    break;
 
             }
         }
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidget();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        wlanReceiver=new WlanReceiver();
+        wlanReceiver.setHandler(uiHandler);
+        registerReceiver(wlanReceiver,filter);
+        isRegReceiver=true;
 
     }
 
-    public void alert(String msg,int time){
-        Toast.makeText(this,msg,time).show();
-    }
 
 
     public void initWidget(){
         editText=findViewById(R.id.input_text);
         search=findViewById(R.id.btn_search);
-        progressBar=findViewById(R.id.progress);
-        progressBar.setVisibility(View.INVISIBLE);
-
+        wlan_state=findViewById(R.id.wifi_state);
         search.setOnClickListener(new View.OnClickListener() {   //按钮点击事件
             @Override
             public void onClick(View v) {
                 String str=editText.getText().toString();
                 if(str==null || str.isEmpty()){
-                    alert("还没有输入办公室",0);
+                    PromptMessage.alert(MainActivity.this,"还没有输入办公室",0);;
                     return;
                 }
                 int room=Integer.parseInt(str);
-                if(initTelnetParam(room)){
-                    new TelnetThread().start();
-                    progressBar.setVisibility(View.VISIBLE);
-                    editText.setEnabled(false);
-                    search.setEnabled(false);
-                }
-
+                initTelnetParam(room);
 
             }
         });
     }
 
-    public boolean initTelnetParam(int room){
+    public void initTelnetParam(int room){
         int floor=room/100;
         String ipPrefix="10.0.0.";
         String ipSurfix=String.valueOf(floor*10);
         if(floor==0 || floor>20){
-            alert("办公室输入错误，请检查",0);
-            return false;
+            PromptMessage.alert(MainActivity.this,"办公室输入错误，请检查",0);;
+            return;
         }
         String host=ipPrefix+ipSurfix;
         String prompt="<"+floor+"FW01";
-        telnetUtil=new TelnetUtil(host,prompt,room);
-        telnetUtil.setHandler(uiHandler);
-        Log.d("MainActivity-Prefix",ipPrefix);
-        Log.d("MainActivity-Surfix",ipSurfix);
-        Log.d("MainActivity-Host",host);
-        Log.d("MainActivity-Prompt",prompt);
-        return true;
+        if(!netWorkTest(host)){
+            PromptMessage.alert(MainActivity.this,"你的网络不能连接到交换机，请换一个重试",1);
+            return;
+        }
+        Intent intent=new Intent(MainActivity.this,ResultActivity.class);
+        intent.putExtra("mHost",host);
+        intent.putExtra("mPrompt",prompt);
+        intent.putExtra("mRoom",room);
+        startActivity(intent);
 
     }
 
-    public void messageBox(String msg,String positi,boolean cancle){
+    public boolean netWorkTest(String ip){
 
-        AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
-        dialog.setMessage(msg);
-        dialog.setPositiveButton(positi, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
+        boolean flag=false;
+        try {
+            Process p = Runtime.getRuntime().exec("ping -c 3 -w 3 " + ip);    //ping3次
+            int status = p.waitFor();
+            if(status==0){
+                flag=true;
             }
-        });
+        } catch (IOException e) {
+            e.printStackTrace();
 
-        dialog.setCancelable(cancle);
-        dialog.show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
 
+        }
+
+        return flag;
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -150,38 +148,39 @@ public class MainActivity extends AppCompatActivity {
                         "QQ:1246454589\n"+
                         "tel:13347511398";
 
-            messageBox(msg,"关闭",false);
+            PromptMessage.messageBox(MainActivity.this,msg,"关闭",false);
         }
         return true;
     }
 
-    private  class TelnetThread extends Thread{
-        @Override
-        public void run() {
-            Log.v("TelnetObject", String.valueOf(telnetUtil==null));
-            if(telnetUtil.connectToSwitch()){
-                ipaddress=telnetUtil.get();
-            }else {
-                return;
-            }
-
-            telnetUtil.disConnect();
-
-            Set<String> keys=ipaddress.keySet();
-            Iterator iterator=keys.iterator();
-            while (iterator.hasNext()){
-                String disc= (String) iterator.next();
-                Log.d("TelnetThread",disc+":"+ipaddress.get(disc));
-            }
-
-            Message message=new Message();
-            message.what=5;
-            uiHandler.sendMessage(message);
-
+    @Override
+    protected void onPause() {
+        if(isRegReceiver){
+            unregisterReceiver(wlanReceiver);
+            isRegReceiver=false;
         }
+        super.onPause();
     }
 
+    @Override
+    protected void onDestroy() {
+        if(isRegReceiver){
+            unregisterReceiver(wlanReceiver);
+            isRegReceiver=false;
+        }
+        super.onDestroy();
+    }
 
+    @Override
+    protected void onResume() {
+        if(!isRegReceiver){
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+            filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+            registerReceiver(wlanReceiver,filter);
+            isRegReceiver=false;
+        }
 
-
+        super.onResume();
+    }
 }
